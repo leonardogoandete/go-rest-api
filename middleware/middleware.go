c507a1bdda9399164994dfc920dbc08ab7d689bb
leonardo.goandete <leonardo.goandete@hotmail.com>
2025-06-09 20:14:20 -0300

    Adicionado middlewares com Content Type e log

---
+++ middleware/middleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func ContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request: %s %s %s", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
+}
--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,15 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"github.com/leonardogoandete/go-rest-api/controllers"
+	"github.com/leonardogoandete/go-rest-api/middleware"
 	"log"
 	"net/http"
 )
 
 func HandleRequests() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
+	r.Use(middleware.LoggingMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("POST")
